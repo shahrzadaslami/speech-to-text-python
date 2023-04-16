833fa29ace2a1734073c913a75011f5d1559bee5
shahrzad aslami <raha.aslami@gmail.com>
2023-04-16 15:17:47 +0430

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+
+filename = "speech.wav"
+
+r = sr.Recognizer()
+
+with sr.AudioFile(filename) as source:
+    #load data to memory
+    audio_data = r.record(source)
+    #recognize
+    text = r.recognize_google(audio_data)
+
+    print(text)
