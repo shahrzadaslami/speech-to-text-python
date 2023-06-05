a7db768445f29fa21955dfcac7a095e6cb4b1ba7
shahrzad aslami <43216706+shahrzadaslami@users.noreply.github.com>
2023-06-05 19:46:29 +0330

    Branch two (#5)
    * main updated
    
    * Co-authored-by: Mahdi Abbaspour Shahmarasi <mahdi.abbaspour.sh@gmail.com>
    
    * changes Co-authored-by: Mahdi Abbaspour Shahmarasi <mahdi.abbaspour.sh@gmail.com>

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from happytransformer import TTSettings
 
 #this code will corect your gramatical mistakes
 
+#changes
+
 happy_tt = HappyTextToText("T5",  "prithivida/grammar_error_correcter_v1")
 text = "gec: " + "we no are open tomorrow"
 settings = TTSettings(do_sample=True, top_k=10, temperature=0.5,  min_length=1, max_length=100)
